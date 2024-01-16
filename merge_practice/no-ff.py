0dab1053be62f44aafa1f8d917d9bdcd335357d9
2ㅣ2ㅣ <csi000821@naver.com>
2024-01-16 13:25:05 +0900

    restart

--- merge_practice/no-ff.py
+++ merge_practice/no-ff.py
@@ -32,5 +32,5 @@ while True:
     )
     print("도봉이>>> ", end="")
     for chunk in dobongi(response):
-        print(chunk, end="")
-    print("\n", end="")
+        print(chunk, end="",flush=True)
+    print()
